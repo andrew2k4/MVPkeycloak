da3f4ed00a23711b704ad0284b2df7c68ca6f0e1
Andrew Jonathan Nguemnang Tchasso <anguemnang-tchasso@recom.eu>
2025-04-25 18:57:49 +0200

    handle refresh token

--- client/src/app/services/interceptor/http-token.interceptor.ts
+++ client/src/app/services/interceptor/http-token.interceptor.ts
@@ -1,22 +1,30 @@
-import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
+import { HttpEventType, HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { KeycloakService } from '../keycloak/keycloak.service';
+import { from, mergeMap, tap } from 'rxjs';
+
 
 export const httpTokenInterceptor: HttpInterceptorFn = (req, next) => {
 
   const keycloakService = inject(KeycloakService);
 
-  const token = keycloakService.keycloak.token;
-  console.log("intercept")
-  if(token){
-    const authReq = req.clone({
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${token}`
-      })
+  return from(
+    keycloakService.keycloak.updateToken(60)
+  ).pipe(
+    mergeMap(()=>{
+      const token = keycloakService.keycloak.token;
+      console.log("intercept")
+      if(token){
+        const authReq = req.clone({
+          headers: req.headers.set(
+            "Authorization", `Bearer ${token}`
+          )
+        })
+        return next(authReq)
+      }
+
+
+      return next(req);
     })
-    return next(authReq)
-  }
-
-
-  return next(req);
+  )
 };
