4ed701bcc751a4f5c4abced483a95cc6e3ee938b
Andrew Jonathan Nguemnang Tchasso <anguemnang-tchasso@recom.eu>
2025-04-25 14:26:35 +0200

    add keycloakService

---
+++ client/src/app/services/keycloak/keycloak.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import Keycloak from 'keycloak-js';
+import { User } from './User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KeycloakService {
+
+  private _keycloak : Keycloak | undefined;
+  private _user: User | undefined;
+
+  get keycloak() {
+      if(!this._keycloak){
+        this._keycloak = new Keycloak({
+          url: 'http://localhost:8080',
+          realm: 'andrew-backend',
+          clientId: 'public-client'
+        })
+      }
+    return this._keycloak;
+  }
+
+  get user(): User | undefined {
+    return this._user;
+
+  }
+
+
+  constructor() { }
+
+  async init(): Promise<any>{
+    const authenticated : boolean =  await this.keycloak?.init({
+      onLoad:'login-required'
+    })
+
+    if(authenticated){
+        this._user = (await this.keycloak?.loadUserProfile()) as User
+        this._user.token = this.keycloak?.token;
+        console.log(this.user)
+    }
+
+  }
+
+
+  login(){
+    return this.keycloak?.login();
+  }
+}
